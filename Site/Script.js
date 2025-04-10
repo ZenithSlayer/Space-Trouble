e018722dcc3c6b0e4db27ca83bb416e9255896b9
Zenith Slayer <miguellurk@gmail.com>
2025-04-10 19:04:30 -0300

    fixed the links

--- Site/Script.js
+++ Site/Script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('a').forEach(anchor => {
+document.querySelectorAll('a.NavLink').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
